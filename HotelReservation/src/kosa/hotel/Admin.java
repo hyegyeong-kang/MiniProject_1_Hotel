0cf24771627b38e68b0678a57702cd38a9f5cbfa
hyegyeong-kang <rkdgprud96@gmail.com>
2023-02-09 00:13:12 +0900

    add reservation, admin, room files

---
+++ HotelReservation/src/kosa/hotel/Admin.java
@@ -0,0 +1,31 @@
+package kosa.hotel;
+
+public class Admin {
+	private String adminName;
+	private String adminNo;
+	
+	public Admin() {}
+
+	public Admin(String adminName, String adminNo) {
+		super();
+		this.adminName = adminName;
+		this.adminNo = adminNo;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getAdminNo() {
+		return adminNo;
+	}
+
+	public void setAdminNo(String adminNo) {
+		this.adminNo = adminNo;
+	}
+
+}
---
+++ HotelReservation/src/kosa/hotel/ReservationManager.java
@@ -0,0 +1,34 @@
+package kosa.hotel;
+
+public class ReservationManager {
+	
+	public ReservationManager() {}
+	
+	public void checkReservation() {
+		//예약 리스트 출력
+	}
+	
+	public void updateReservation() {
+		//수정할 예약 선택
+		//데이터 업데이트
+	}
+	
+	public void cancelReservation() {
+		// 취소할 예약 선택
+		// 리스트에서 삭제
+	}
+	
+	public void checkIn(String reservationNo) {
+		// 예약 번호를 입력받아서 예약리스트에 해당 예약번호가 있다면 
+		// 체크인 완료 
+	}
+	
+	public void checkOut(String roomNum) {
+		// 객실번호를 받아서 해당 객실 번호의 예약 상태 업데이트(다시 예약할 수 있는 방으로 돌아가는 것)
+	}
+	
+
+	
+
+	
+}
